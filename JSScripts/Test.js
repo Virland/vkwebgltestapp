8cac35c5a242bb9f4b7e57d88fa98f656401b59f
XZ <maksim.matosov@academmedia.com>
2015-11-16 15:35:10 +0500

    vk item purchase test

---
+++ JSScripts/Test.js
@@ -0,0 +1,47 @@
+﻿VK.init(function() {
+	VK.addCallback("onApplicationAdded", function{});
+	VK.addCallback("onSettingsChanged", function{});
+	VK.addCallback("onBalanceChanged", function{});
+    VK.addCallback("onOrderCancel", HandleOrderCallback);
+    VK.addCallback("onOrderSuccess", HandleOrderCallback);
+    VK.addCallback("onOrderFail", HandleOrderCallback);
+    VK.addCallback("onProfilePhotoSave", function{});
+    VK.addCallback("onWallPostSave", function{});
+	VK.addCallback("onWallPostCancel", function{});
+	VK.addCallback("onWindowResized", function{});
+	VK.addCallback("onLocationChanged", function{});
+	VK.addCallback("onWindowBlur", function{});
+	VK.addCallback("onWindowFocus", function{});
+	VK.addCallback("onScrollTop", function{});
+	VK.addCallback("onScroll", function{});
+	VK.addCallback("onToggleFlash", function{});
+	//VK.addCallback("", function{});
+  });
+
+function BuyGoldd () {
+	if(VK !== undefined) {
+		var params {
+			type: "item",
+			item: "kusok_gavna"
+		};
+		VK.showOrderBox();
+	}
+	SendMessage ("JSConnector", "GetMoney", 10);
+}
+
+function SendUserNameToUnity () {
+	SendMessage ("JSConnector", "SetUserName", "GAVNO");
+}
+
+function HandleOrderCallback(data) {
+	if("orderId" in data) {
+		//Success
+		SendMessage ("JSConnector", "ShowPurchaseResult", "Success");
+	} else if("errorCode" in data) {
+		//Error
+		SendMessage ("JSConnector", "ShowPurchaseResult", "Error : " + data.errorCode);
+	} else {
+		//Cancel
+		SendMessage ("JSConnector", "ShowPurchaseResult", "Canceled");
+	}
+}
